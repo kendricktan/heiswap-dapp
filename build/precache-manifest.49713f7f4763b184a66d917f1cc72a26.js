279fe273b80757d0e300047e29be2e50ce38c9e4
Kendrick Tan <kendricktan0814@gmail.com>
2019-07-06 13:25:02 +1000

    netlify

---
+++ build/precache-manifest.49713f7f4763b184a66d917f1cc72a26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7290aa95c2ff3ffa0ae5cdf28b0a533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d232430917871f7c6e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ecd158f27a2f85be33d0",
+    "url": "/static/js/2.0cb420df.chunk.js"
+  },
+  {
+    "revision": "74d232430917871f7c6e",
+    "url": "/static/js/main.2038eba2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ timeline.md
@@ -0,0 +1,8 @@
+# Heiswap Rough Timeline
+
+Jul 2019 - MVP, feedback
+Aug 2019 - Iterate, fix bugs, optimize data storage(?)
+Sep 2019 - Add types to frontend, refactor to make it more maintainable
+Oct 2019 - Write tests for Solidity and frontend + CI/CD pipeline
+Nov 2019 - Release stable version
+Dec 2019 - Read more on SNARKs / zk-rollups on how to integrate them
